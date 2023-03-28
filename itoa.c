708e9c61b02242de69544aa6fdb1d3168e42098b
sohilaabdalla1laa <sohilaabdalla113@gmail.com>
2023-03-28 04:22:31 -0700

    done

--- itoa.c
+++ itoa.c
@@ -8,26 +8,26 @@
  */
 char *convert(long int num, int base)
 {
-        static char *array = "0123456789abcdef";
-        static char buffer[50];
-        char sign = 0;
-        char *ptr;
-        unsigned long n = num;
+	static char *array = "0123456789abcdef";
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
 
-        if (num < 0)
-        {
-                n = -num;
-                sign = '-';
-        }
-        ptr = &buffer[49];
-        *ptr = '\0';
+	if (num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	ptr = &buffer[49];
+	*ptr = '\0';
 
-        do      {
-                *--ptr = array[n % base];
-                n /= base;
-        } while (n != 0);
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
 
-        if (sign)
-                *--ptr = sign;
-        return (ptr);
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
 }
